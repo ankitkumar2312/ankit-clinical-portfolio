d4213656a0f41b169cc0155611ca6e43094dc62c
ankitkumar2312 <ankitkumar8130260@gmail.com>
2026-07-31 22:04:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 console.log("Portfolio Loaded Successfully 🚀");
+// Scroll Reveal Animation
+
+const hiddenElements = document.querySelectorAll(
+".about, .studies, .skills, .experience, .contact"
+);
+
+const observer = new IntersectionObserver((entries)=>{
+
+    entries.forEach(entry=>{
+
+        if(entry.isIntersecting){
+
+            entry.target.classList.add("show");
+
+        }
+
+    });
+
+},{
+    threshold:0.2
+});
+
+hiddenElements.forEach(el=>{
+
+    el.classList.add("hidden");
+
+    observer.observe(el);
+
+});
