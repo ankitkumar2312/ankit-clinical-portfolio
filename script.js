50479329be8184ad18b4856c0d2c4dc263709dbf
ankitkumar2312 <ankitkumar8130260@gmail.com>
2026-08-04 21:17:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -146,3 +146,13 @@ backToTop.addEventListener("click", () => {
     });
 
 });
+// Mobile Menu
+
+const menuToggle = document.querySelector(".menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+menuToggle.addEventListener("click", () => {
+
+    navLinks.classList.toggle("active");
+
+});
